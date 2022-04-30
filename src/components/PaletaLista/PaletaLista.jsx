ab87188e2c97b7337eddbd49aadcc6436643931e
LHcouto <xluisx_couto@hotmail.com>
2022-04-30 17:59:25 -0300

    alterando arquitetura de pastas

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { paletas } from "../mocks/paletas.js";
+import { paletas } from "mocks/paletas.js";
 import './PaletaLista.css';
 
 function PaletaLista() {
